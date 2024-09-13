a8c91e147145e1bfffc85cd7aefdca13052b2bf2
uwi0 <kakapoweb@gmail.com>
2024-09-13 15:58:57 +0700

    Initial Commit

---
+++ Activity Tracker/Activity_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Activity_TrackerApp.swift
+//  Activity Tracker
+//
+//  Created by dwi prasetyo on 13/09/24.
+//
+
+import SwiftUI
+
+@main
+struct Activity_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
